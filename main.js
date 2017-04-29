4bc4dd6fb3c55822adca055086a6f2ac1c8bc556
Carl Halbreiner <chalbrei@oberlin.edu>
2017-04-29 13:27:06 -0400

    updated main for builder

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder')
 
 module.exports.loop = function() {
     
@@ -6,6 +7,8 @@ module.exports.loop = function() {
         var creep = Game.creeps[name];
         if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
+        } else if(creep.memory.role == 'builder'){
+            roleBuilder.run(creep);
         }
     }
     
